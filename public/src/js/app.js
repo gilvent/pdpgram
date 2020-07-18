574e2470c905599caa0ccafb33aeb09e086e616d
Alvaro <alvaro@gdn-commerce.com>
2020-07-18 11:34:15 +0700

    Add initial listeners in service workers

---
+++ public/src/js/app.js
@@ -0,0 +1,22 @@
+var deferredPrompt;
+
+if(!window.Promise) {
+  window.Promise = Promise;
+}
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .register('/sw.js')
+    .then(function() {
+      console.log('Service worker registered!');
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
+}
+
+window.addEventListener('beforeinstallprompt', function(event) {
+  event.preventDefault();
+  deferredPrompt = event;
+  return false;
+})
