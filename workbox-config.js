ce2388934619a20248ef5bc77b0c57a93b4f860d
gilvent <alvarolukmanto@gmail.com>
2020-11-29 21:21:51 +0700

    configure workbox using workbox-cli

---
+++ workbox-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  "globDirectory": "public/",
+  "globPatterns": [
+    "**/*.{html,ico,json,css,js}",
+    "src/images/*.{png,jpg}"
+  ],
+  "swDest": "public/service-worker.js",
+  "globIgnores": [
+    "help/**"
+  ]
+};
