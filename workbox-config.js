093fc02f12f5327ecb67124aecacf4ee511f0d34
gilvent <alvarolukmanto@gmail.com>
2021-01-07 12:13:24 +0700

    add workbox-sw package to the project

--- workbox-config.js
+++ workbox-config.js
@@ -4,8 +4,14 @@ module.exports = {
     "**/*.{html,ico,json,css,js}",
     "src/images/*.{png,jpg}"
   ],
+  "swSrc": "public/sw-base.js",
   "swDest": "public/service-worker.js",
   "globIgnores": [
-    "help/**"
+    "help/**",
+    /**
+     * Ignore workbox-sw because it is automatically cached
+     * Ref: https://developers.google.com/web/tools/workbox/modules/workbox-sw
+     */
+    "lib/workbox-v5.1.4" 
   ]
 };
