53298bba767c2363cdc061aa77fb3cc611d40758
gilvent <alvarolukmanto@gmail.com>
2021-01-07 13:17:00 +0700

    move post images to different cache by register new workbox route

--- public/sw-base.js
+++ public/sw-base.js
@@ -3,9 +3,23 @@ importScripts('/lib/workbox-v5.1.4/workbox-sw.js');
 workbox.setConfig({modulePathPrefix: '/lib/workbox-v5.1.4/'})
 
 workbox.routing.registerRoute(
+  new RegExp(/.*(?:firebasestorage\.googleapis)\.com.*$/),
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: 'post-images'
+  })
+)
+
+workbox.routing.registerRoute(
   new RegExp(/.*(?:googleapis|gstatic)\.com.*$/),
   new workbox.strategies.StaleWhileRevalidate({
     cacheName: 'google-fonts'
+  })
+)
+
+workbox.routing.registerRoute(
+  'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css',
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: 'material-css'
   })
 )
 
